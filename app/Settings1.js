ce707a9f8260f885f3f6c07adbc28433dfd8b1a3
vinui409 <vinu.i2017@gmail.com>
2023-11-15 19:37:25 -0800

    js for the first settings

---
+++ app/Settings1.js
@@ -0,0 +1,15 @@
+// Placeholder user information
+
+var user_full_name = "Andy"; // Ideally, this would come from a server or database
+var user_username = "AndyUser";
+var user_email = "andy@example.com";
+var password = "Andy1234";
+var bio = "verginia beach";
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('name').value = user_full_name;
+    document.getElementById('username').value = user_username;
+    document.getElementById('email').value = user_email;
+    document.getElementById('password').value = password;
+    document.getElementById('bio').value = bio;
+});
